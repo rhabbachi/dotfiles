9c3c9a61f859b21a007f49b8c849cf5a1d70035e
Riadh Habbachi <riadh@angrycactus.biz>
2015-01-03 20:54:39 +0100

    First batch of dotfiles.

---
+++ home/.drush/settings.commons.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Set some useful development environment variables
+*/
+
+//$conf array are drupal variables that cannot be overridden by db settings.
+$conf['file_directory_temp'] = "sites/default/files/tmp";
+$conf['file_directory_path'] = "sites/default/files";
+
+$conf['file_public_path'] = "sites/default/files/public";
+$conf['file_private_path'] = "sites/default/files/private";
+$conf['file_temporary_path'] = "sites/default/files/tmp";
+
+# turn off caching & aggregation
+$conf['cache'] = FALSE;
+$conf['block_cache'] = FALSE;
+$conf['preprocess_css'] = FALSE;
+$conf['preprocess_js'] = FALSE;
+
+# toggle module settings
+# $conf['apachesolr_read_only'] = 1;
+# $conf['apc_enable'] = FALSE;
+
+# enable developer modules
+$conf['devel_enable'] = TRUE;
+$conf['devel_themer_enable'] = TRUE;
+
+# Increase some php variables
+ini_set('memory_limit', '512M');
+ini_set('realpath_cache_size', '24M');
+
+# display all error messages
+//$conf['error_level'] = 2;
+//ini_set('display_errors', TRUE);
+//ini_set('display_startup_errors', TRUE);
+
+# Mandrill
+$conf['mandrill_api_key'] = 'eRvS_swm1JwZwXvB28hsBA';
+# intercept emails using Devel
+$conf['mail_system'] = array(
+	//'default-system' => 'DevelMailLog',
+	'default-system' => 'MandrillMailSystem',
+);
+
+// Uncomment the following to disable Secure Pages
+$conf['securepages_enable'] = FALSE;
+
+// Whether the Environment Indicator should use the settings.php variables for
+// the indicator. On your production environment, you should probably set this
+// to FALSE.
+$conf['environment_indicator_overwrite'] = TRUE;
+
+// The text that will be displayed on the indicator.
+$conf['environment_indicator_overwritten_name'] = 'localhost';
+
+/**
+ *  * Reroute Email module:
+ *   *
+ *    * To override specific variables and ensure that email rerouting is
+ *    enabled or
+ *     * disabled, change the values below accordingly for your site.
+ *      */
+// Enable email rerouting.
+$conf['reroute_email_enable'] = 1;
+// Space, comma, or semicolon-delimited list of email addresses to pass
+// through. Every destination email address which is not on this list will be
+// rerouted to the first address on the list.
+$conf['reroute_email_address'] = "riadh+rerouted@angrycactus.biz";
+// Enable inserting a message into the email body when the mail is being
+// rerouted.
+$conf['reroute_email_enable_message'] = 1;
+
+// admin_menu
+$conf['admin_menu_position_fixed'] = 0;
+$conf['admin_menu_components'] =  array (
+  'admin_menu.icon' => true,
+  'admin_menu.menu' => true,
+  'admin_menu.search' => false,
+  'admin_menu.users' => true,
+  'admin_menu.account' => true,
+  'shortcut.links' => false,
+);
+
+// module_filter
+$conf['module_filter_use_switch'] = 0;
---
+++ home/.zshrc
@@ -0,0 +1,131 @@
+# Enable correct colors
+[[ "$COLORTERM" == "gnome-terminal" ]] && export TERM=xterm-256color
+
+# Base16 Shell
+BASE16_SCHEME="monokai"
+BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+
+# Better prompt
+source $HOME/.promptline.sh
+
+# Antigen
+source $HOME/.antigen/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundles <<EOBUNDLES
+command-not-found
+colored-man
+encode64
+zsh_reload
+colorize
+rsync
+
+aws
+
+git
+gitfast
+git-extras
+git-flow
+gitignore
+svn
+
+pip
+
+npm
+node
+
+vundle
+
+vagrant
+bundler
+gem
+rvm
+knife
+
+docker
+
+composer
+# Syntax highlighting bundle.
+zsh-users/zsh-syntax-highlighting
+
+# More completions
+zsh-users/zsh-completions src
+EOBUNDLES
+
+antigen bundle fasd
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
+
+antigen bundle debian
+export apt_pref="apt-get"
+unalias ag
+
+[[ "$COLORTERM" == "gnome-terminal" ]] && export ZSH_TMUX_AUTOSTART=true
+[[ "$COLORTERM" == "gnome-terminal" ]] && export ZSH_TMUX_AUTOQUIT=true
+antigen bundle tmux
+
+# Common aliases
+antigen bundle common-aliases
+alias t="multitail -f"
+alias a2log="multitail -f /var/log/apache2/localhost.error.log /var/log/apache2/localhost.access.log"
+
+# Tell antigen that you're done.
+antigen apply
+
+# PV
+alias -g PV="| pv -ptrab |"
+
+# Drush
+[[ -f ~/.zshrc.drush ]] && source $HOME/.zshrc.drush
+
+# Open file with the right application
+function open () {
+ setsid xdg-open $1
+}
+
+# Tree
+alias trees='tree -L 3 | less'
+
+# AUTOSSH
+alias ssh='autossh -M 0 -o "ServerAliveInterval 45" -o "ServerAliveCountMax 2"'
+
+# TOP
+alias top="htop"
+
+# Ping www.google.com 32 times
+alias pingo="ping www.google.com -c 8"
+
+alias date-time='date +%Y%m%d-%H%M%S'
+
+# Homeshick
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+fpath=($HOME/.homesick/repos/homeshick/completions $fpath)
+
+alias clr="clear"
+
+# Ag
+ajs() {
+    ag --js "$@"
+}
+
+acss() {
+    ack --saas --css --less "$@"
+}
+
+fjs() {
+    ag --js -l "$@"
+}
+
+fcss() {
+    ack --saas --css --less -l "$@"
+}
