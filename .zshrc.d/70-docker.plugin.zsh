e192e07eb8459b6fc0abb947c870d5d11ae236c1
Riadh Habbachi <habbachi.riadh@gmail.com>
2020-11-16 18:06:40 +0100

    chore(zsh): cleanup docker prune aliases

--- .zshrc.d/70-docker.plugin.zsh
+++ .zshrc.d/70-docker.plugin.zsh
@@ -1,12 +1,6 @@
 #https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes
-function docker-rmdangling-img() {
-  docker rmi $(docker images -f dangling=true -q)
-}
-
-function docker-rmexited() {
-  docker rm $(docker ps -a -f status=exited -q)
-}
-
-function docker-rmdangling-vlm() {
+function docker-prune-vlm() {
   docker volume rm $(docker volume ls -f dangling=true -q)
 }
+
+alias ,docker-system-prune="{ docker system prune all ; docker-prune-vlm ; }"
