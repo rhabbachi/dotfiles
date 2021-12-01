d79d480181da520daa2dd5083b61b47f228fa884
Riadh Habbachi <habbachi.riadh@gmail.com>
2021-12-01 12:14:35 +0100

    feat(zsh): improve custom yay upgrade command

--- .zshrc.d/60-common.plugin.arch.zsh
+++ .zshrc.d/60-common.plugin.arch.zsh
@@ -3,8 +3,24 @@ if command -v pacaur >/dev/null 2>&1; then
   # alias pawsu="pacaur -Sy --noconfirm && pacaur -Suwr --noconfirm && PKGDEST=\"/home/rhabbachi/.local/cache/pacman/pkg/\" pacaur -Suwa --noconfirm"
 fi
 
+function ,yayrmorphans () {
+  orphned=$(yay -Qtdq)
+
+  if [[ ! -z $orphned ]]; then
+    yay -Rs $orphned
+  fi
+}
+
 if command -v yay >/dev/null 2>&1; then
-  alias ,yuaywr="yay -Syu --noconfirm --aur --devel --answerupgrade Repo --answerclean None --answerdiff None && yay -Syuw --repo --answerupgrade None --noconfirm"
+  cmd_aur="yay -Syu --noconfirm --aur --devel --answerupgrade Repo --answerclean None --answerdiff None"
+
+  if command -v pacman-offline >/dev/null 2>&1; then
+    cmd_repo="sudo pacman-offline -y"
+  else
+    cmd_repo="yay -Syuw --repo --answerupgrade None --noconfirm"
+  fi
+
+  alias ,yuaywr="$cmd_aur; $cmd_repo; sudo paccache -rvk2; ,yayrmorphans"
 fi
 
 # GIT_CONTRIB is needed for git contrib scripts.
